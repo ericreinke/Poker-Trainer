2de63e8f87e019812c2967ae6a6905e04ea5e34c
Eric <ericcreinke@gmail.com>
2018-01-23 03:28:03 -0500

    recommit

---
+++ app/src/main/java/eric/example/com/pokertrainer/MainActivity.java
@@ -0,0 +1,47 @@
+package eric.example.com.pokertrainer;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button handRankBtn= (Button)findViewById(R.id.handRankBtn);
+        handRankBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent handRankIntent = new Intent(getApplicationContext(),SecondActivity.class);
+
+                startActivity(handRankIntent);
+            }
+        });
+
+        final Button startingHandsBtn = (Button)findViewById(R.id.startingHandsBtn);
+        startingHandsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startingHandsIntent = new Intent(getApplicationContext(),ThirdActivity.class);
+
+                startActivity(startingHandsIntent);
+            }
+        });
+
+        final Button positioningBtn = (Button)findViewById(R.id.positioningBtn);
+        positioningBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent positioningIntent = new Intent(getApplicationContext(),ThirdActivity.class);
+
+                startActivity(positioningIntent);
+            }
+        });
+
+    }
+}
